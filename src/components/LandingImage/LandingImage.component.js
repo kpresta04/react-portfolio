11a7f631c54a8fb7bb0bc7567b555673d7647cfc
Kellen Presta <kpresta04@gmail.com>
2020-04-29 18:46:09 -0500

    image text

--- src/components/LandingImage/LandingImage.component.js
+++ src/components/LandingImage/LandingImage.component.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./landingImage.scss";
+import { Link } from "react-router-dom";
 
 export default function LandingImage() {
 	return (
@@ -17,6 +18,17 @@ export default function LandingImage() {
     ./assets/images/comp.jpg"
 				></img>
 			</picture>
+			<div className="welcome-text">
+				<h1 id="welcomeBenefit" className="leading">
+					I bring your projects to life
+				</h1>
+				<h3 id="welcomeSubtitle">Full Stack Engineer</h3>
+				<Link to="/portfolio">
+					<button id="welcomeButton" className="button">
+						View My Work
+					</button>
+				</Link>
+			</div>
 		</div>
 	);
 }
