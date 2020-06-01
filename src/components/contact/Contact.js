4464204dbea1e9a492147e216317ef65e4081c45
Kellen Presta <kpresta04@gmail.com>
2020-06-01 16:25:10 -0500

    contact page changed

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,38 +1,51 @@
 import React from "react";
 import "./contact.scss";
 import HeroText from "../HeroText/HeroText.component";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+	root: {
+		width: "100%",
+		maxWidth: 800,
+		minHeight: "50vh",
+		justifyContent: "center",
+		textAlign: "center",
+		margin: "0 auto",
+	},
+
+	links: {
+		textDecoration: "underline",
+
+		"&:hover": {
+			color: "#3f51b5",
+		},
+	},
+});
 
 export default function Contact() {
+	const classes = useStyles();
 	return (
 		<div className="fade-in-fast">
 			<section id="contact">
 				<HeroText text="Get in Touch" />
 
-				<form
-					id="contactForm"
-					onSubmit={(e) => {
-						e.preventDefault();
-						console.log("submitted");
-					}}
-				>
-					<input name="name" type="text" placeholder="NAME" />
-					<input name="email" type="email" placeholder="EMAIL" />
-					<textarea
-						name="message"
-						placeholder="YOUR MESSAGE"
-						cols="30"
-						rows="10"
-					></textarea>
-					<button id="sendBtn" className="button">
-						Send
-						<img
-							style={{ height: "1em" }}
-							id="planeImg"
-							alt="Send Button"
-							src="./assets/images/planeHover.jpg"
-						></img>
-					</button>
-				</form>
+				<div className={classes.root}>
+					<Typography variant="h4" gutterBottom>
+						Send me an{" "}
+						<a className={classes.links} href="mailto:kpresta04@gmail.com">
+							email
+						</a>
+						{", "}
+						or connect on{" "}
+						<a
+							className={classes.links}
+							href="https://www.linkedin.com/in/kellen-presta-986215198/"
+						>
+							LinkedIn.
+						</a>
+					</Typography>
+				</div>
 			</section>
 		</div>
 	);
